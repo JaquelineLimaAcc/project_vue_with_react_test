3cee6406b51c74ac58e36d4d635550f517f5eba9
Leonardo Lemos <lemos@example.com>
2025-09-16 15:46:27 +0100

    Fixing connections errors

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,20 +1,5 @@
 import sql from 'mssql';
 
-
-/*
-const { Pool } = sql;
-
-const pool = new Pool({
-  user: process.env.MS_USER,
-  host: process.env.MS_HOST,
-  database: process.env.MS_DB,
-  password: process.env.MS_PASSWORD,
-  port: 1433,
-});
-
-export default pool;
-*/
-
 const config = {
   server: process.env.MS_HOST, 
   port: Number(process.env.MS_PORT),
@@ -24,11 +9,10 @@ const config = {
   options: {
     encrypt: true,
     trustServerCertificate: true,     
-    cryptoCredentialsDetails: { minVersion: 'TLSv1.2' } 
   },
   pool: { max: 10, min: 1, idleTimeoutMillis: 30000 }
 };
 
 const pool = await sql.connect(config);
 
-export default pool; 
+export default pool;
